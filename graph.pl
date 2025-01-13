5eebd5a7ee66d9a4fb1f03f973f6f5fb44610524
JayashBhattarai <109951633+JayashBhattarai@users.noreply.github.com>
2025-01-13 21:40:29 +0545

    Update graph.pl

--- graph.pl
+++ graph.pl
@@ -1,36 +1,40 @@
+% Define edges
 edge(a, b).
 edge(b, c).
 edge(a, d).
 edge(b, d).
 edge(d, e).
 
+% Define neighbour relationship (undirected graph)
 neighbour(X, Y) :-
     edge(X, Y); edge(Y, X).
 
+% Find a path between two vertices
 path(X, Y, Path) :-
     path(X, Y, [X], Path).
 
+path(X, X, PPath, PPath).
 path(X, Y, PPath, FPath) :-
     neighbour(X, Z),
     \+(member(Z, PPath)),
     path(Z, Y, [Z|PPath], FPath).
 
-path(X, X, PPath, PPath).
-
-
-/ find all cycles starting from a given vertex in a graph /
+% Find all cycles starting from a given vertex
 find_cycle(X, Cycle) :-
     find_cycle(X, X, [X], Cycle).
 
-
 find_cycle(X, Y, Path, Cycle) :-
     neighbour(Y, Z),
-    \+(member(Z, Path)), 
+    \+(member(Z, Path)),
     find_cycle(X, Z, [Z|Path], Cycle).
 
- 
+find_cycle(X, Y, Path, Cycle) :-
+    neighbour(Y, X), % If we return to the start node, it's a cycle
+    Cycle = [X|Path].
 
-
-/ find the shortest path between a pair of vertices /
-/ shortest_path(Start, End, Path) :-
-/
+% Find the shortest path between two vertices
+shortest_path(Start, End, Path) :-
+    setof(Length-TempPath, (
+        path(Start, End, TempPath),
+        length(TempPath, Length)
+    ), [MinLength-Path|_]).
